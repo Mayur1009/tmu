e6813b95f9d3758d13b590d3445194f984ff0d88
Mayur1009 <mayur.k.shende@gmail.com>
2024-05-18 13:01:05 +0200

    to_cpu(): gg

--- tmu/models/classification/multioutput_classifier.py
+++ tmu/models/classification/multioutput_classifier.py
@@ -363,10 +363,14 @@ class TMCoalesceMultiOuputClassifier(TMBaseModel, SingleClauseBankMixin, MultiWe
 
         if self.platform in ["GPU", "CUDA"]:
             clause_bank_gpu = self.clause_bank
-            self._profiler.profile(cuda.memcpy_dtoh, self.clause_bank, self.clause_bank_gpu)
             clause_bank_type, clause_bank_args = self._build_cpu_bank(X)
             clause_bank_cpu = clause_bank_type(**clause_bank_args)
-            clause_bank_cpu.clause_bank = np.copy(clause_bank_gpu.clause_bank)
+            
+            clause_bank_cpu.clause_bank = clause_bank_gpu.clause_bank
+            clause_bank_cpu.clause_output = clause_bank_gpu.clause_output
+            clause_bank_cpu.literal_clause_count = clause_bank_gpu.literal_clause_count
+
+            clause_bank_cpu._cffi_init()
 
             self.clause_bank = clause_bank_cpu
             self.platform = "CPU"
